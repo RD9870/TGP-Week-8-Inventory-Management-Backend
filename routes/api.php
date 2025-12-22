e6844da00080127488777d055af5a60a9bf00144
RD9870 <reemfarajd@gmail.com>
2025-12-22 22:38:46 +0200

    changed api

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckUserRole;
 use App\Http\Controllers\AuthController;
 // use App\Http\Controllers\AuthController;
-// use App\Http\Controllers\CategoryController;
-// use App\Http\Controllers\SubcategoryController;
-// use App\Http\Controllers\UserController;
-// use App\Http\Middleware\AdminMiddleware;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SubcategoryController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminMiddleware;
 // use App\Http\Middleware\ManagerMiddleware;
 
 Route::get('/user', function (Request $request) {
@@ -28,7 +28,25 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::put('products/{id}', [ProductController::class, 'update'])->middleware(CheckUserRole::class.':admin');
 
     Route::delete('products/{id}', [ProductController::class, 'destroy'])->middleware(CheckUserRole::class.':admin');
+});
 
+//admin routes
+Route::prefix('admin')->middleware(['auth:sanctum',AdminMiddleware::class])->group(function(){
+    // //rout that gets all books and if the request has a cat param only books with that cat is shown
+    // Route::apiResource('book',CustomerBookController::class)->only(['index','show']);
+    // //route to show all the cart items and remove a cart item
+    // Route::apiResource('cart',CartController::class)->except('store');
+    // //route to add new book to cart
+    // Route::post('cart/{book}',[CartController::class,'store']);
+    // //route to view past orders
+    // Route::apiResource('orders',OrderController::class);
+
+    //User crud route
+        Route::apiResource('users',UserController::class);
+    //category crud route
+        Route::apiResource('categories',CategoryController::class);
+    //subcategory crud route
+        Route::apiResource('subcategories',SubcategoryController::class);
 });
 
 
