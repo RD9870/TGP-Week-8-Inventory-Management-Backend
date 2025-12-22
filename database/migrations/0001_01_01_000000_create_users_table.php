13fcf0937df53bdaa15714c158c58dea0bf473e8
jojolaayss <layasnajah4@gmail.com>
2025-12-22 10:42:08 +0200

    user_table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->decimal('salary', 10, 2);
+            $table->enum('type', ['admin', 'manager', 'cashier']);
             $table->rememberToken();
             $table->timestamps();
         });
