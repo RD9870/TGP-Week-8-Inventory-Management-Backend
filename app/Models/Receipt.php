78047055ae50526caa916150fe6c16f104d40213
RD9870 <reemfarajd@gmail.com>
2025-12-23 09:39:31 +0200

    fixed merge conflict

--- app/Models/Receipt.php
+++ app/Models/Receipt.php
@@ -11,14 +11,8 @@ class Receipt extends Model
     'cashier_id',
     'total',
     ];
-<<<<<<< HEAD
-    public function items(){
-        return $this->hasMany(Receipt_items::class);
-    }
-=======
     public function items()
 {
     return $this->hasMany(Receipt_items::class, 'recipt_id');
 }
->>>>>>> 9d91f7500b2d66c4e70153b773c00b70cad73126
 }
